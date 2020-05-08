ce87c5c0375ecc335cad9fc20f4bc9ea67697e0c
Tshegofatso12-ai <62068842+Tshegofatso12-ai@users.noreply.github.com>
2020-05-08 10:13:01 +0200

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,94 @@
+/*!
+ * Guim González - Portfolio v1.0.0 (https://guimgonzalez.me)
+ * Copyright 2019 Guim González
+ * Licensed under MIT (https://github.com/GuimG/dev-portfolio/master/LICENSE)
+ */
+
+// Change your profile data here
+let profile = {
+  picture: "",
+  name: "Tshegofatso",
+  description: "Fullstack Developer",
+  bio:
+    "I am a third year Computer Science student. I design and develop sophisticated websites. I am passionate about technology and I am always eager to work on new projects",
+  email: "u18189424@tuks.co.za",
+  location: "Pretoria, South Africa",
+  birth: "1999-03-12", // YYYY-MM-DD format
+  skills: [
+    "C++",
+    "CSS",
+    "Java",
+    "HTML",
+    "Javascript",
+    "Firbase",
+    "Python",
+    "PHP",
+    "React",
+    "SQL",
+  ],
+  social: {
+    github: "https://github.com/Tshegofatso12-ai",
+    dev: "https://thetshegofatso.com/"
+  },
+  experience: [
+    {
+      title: "Web Developer",
+      company: "Motech",
+      url: "https://motech.dev/",
+      location: "Johannesburg, South Africa",
+      description: "Writing well designed, testable, efficient code by using best software development practices",
+      from: "March 2020",
+      to: "Now"
+    },
+    {
+      title: "Web Developer and Graphic Designer",
+      company: "Glo-Mo Solutions",
+      url: "https://glomosolutions.co.za/",
+      location: "Johannesburg, South Africa",
+      description: "Create visual concepts, by hand or using computer software, to communicate ideas that inspire, inform, or captivate consumers.",
+      from: "November 2019",
+      to: "Now"
+    },
+    {
+      title: "Retail Sales Assistant ",
+      company: "Gen-X, The Grove",
+      url: "http://thegrovemall.co.za/the-grove-mall---store-listing.html",
+      location: "Pretoria, South Africa",
+      description: "Pursued resolutions to achieve complete customer satisfaction.",
+      from: "November 2018",
+      to: "December 2018"
+    },
+    {
+      title: "Photographer ",
+      company: "Soshanguve Crossing",
+      url: "https://www.soshanguvecrossingmall.co.za/",
+      location: "Pretoria, South Africa",
+      description: "Create visual concepts, by hand or using computer software, to communicate ideas that inspire, inform, or captivate consumers.",
+      from: "December 2017",
+      to: "December 2017"
+    }
+  ],
+  education: [
+    {
+      school: "University of Pretoria",
+      degree: "BSc Computer Science",
+      fieldOfStudy: "",
+      location: "Pretoria, South Africa",
+      from: "January 2018",
+      to: "Now"
+    }
+  ],
+  quote: "Discipline is the best tool."
+};
+
+let page = {
+  language: "english",
+  // ^ languages suported:
+  //  - english (default)
+  theme: "nightsky"
+  // ^ themes suported:
+  //  - nightsky (default)
+  //  - sunset
+  //  - purpledream
+  //  - cutekitty
+};
